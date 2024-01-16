57394eb00ada200a66b7c9b7e80a25c22e67c593
Aamish <aamish@safetEQ.ai>
2024-01-16 20:26:19 +0500

    My code

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -24,7 +24,9 @@ export const TodoList: React.FC = () => {
   const [input, setInput] = useState<string>("");
 
   const [lastId, setLastId] = useState(0);
-  const [todos, setTodos] = useState<Item[]>([]);
+  const [todos, setTodos] = useState<Item[]>([
+    {"id":1,"todo":"Do something nice for someone I care about","completed":true},{"id":2,"todo":"Memorize the fifty states and their capitals","completed":false},{"id":3,"todo":"Watch a classic movie","completed":false},{"id":4,"todo":"Contribute code or a monetary donation to an open-source software project","completed":false},{"id":5,"todo":"Solve a Rubik's cube","completed":false},{"id":6,"todo":"Bake pastries for me and neighbor","completed":false},{"id":7,"todo":"Go see a Broadway production","completed":false},{"id":8,"todo":"Write a thank you letter to an influential person in my life","completed":true},{"id":9,"todo":"Invite some friends over for a game night","completed":false},{"id":10,"todo":"Have a football scrimmage with some friends","completed":false},{"id":11,"todo":"Text a friend I haven't talked to in a long time","completed":false},{"id":12,"todo":"Organize pantry","completed":true},{"id":13,"todo":"Buy a new house decoration","completed":false},{"id":14,"todo":"Plan a vacation I've always wanted to take","completed":false},{"id":15,"todo":"Clean out car","completed":false},{"id":16,"todo":"Draw and color a Mandala","completed":true},{"id":17,"todo":"Create a cookbook with favorite recipes","completed":false},{"id":18,"todo":"Bake a pie with some friends","completed":false},{"id":19,"todo":"Create a compost pile","completed":true},{"id":20,"todo":"Take a hike at a local park","completed":true},{"id":21,"todo":"Take a class at local community center that interests you","completed":false},{"id":22,"todo":"Research a topic interested in","completed":false},{"id":23,"todo":"Plan a trip to another country","completed":true},{"id":24,"todo":"Improve touch typing","completed":false},{"id":25,"todo":"Learn Express.js","completed":false},{"id":26,"todo":"Learn calligraphy","completed":false},{"id":27,"todo":"Have a photo session with some friends","completed":false},{"id":28,"todo":"Go to the gym","completed":false},{"id":29,"todo":"Make own LEGO creation","completed":false},{"id":30,"todo":"Take cat on a walk","completed":false}
+  ]);
   const [currentTodo, setCurrentTodo] = useState("");
   const [editTodo, setEditTodo] = useState("");
 
@@ -118,7 +120,6 @@ export const TodoList: React.FC = () => {
 
 
   useEffect(() => {
-    // Load todos from local storage
     const savedTodos = localStorage.getItem("todos");
     let parsedTodos = JSON.parse(savedTodos || "[]");
     if (parsedTodos.length) {
@@ -126,7 +127,6 @@ export const TodoList: React.FC = () => {
       setLastId(parsedTodos[parsedTodos.length - 1].id);
     }
 
-    // Fetch todos from the API
     const fetchData = async () => {
       try {
         const response = await fetch('https://dummyjson.com/todos');
@@ -135,7 +135,6 @@ export const TodoList: React.FC = () => {
         }
         const data = await response.json();
         setTodos(updateTodos(todos, data));
-        
       } catch (error) {
         console.error('Error fetching data:', error);
       }
